ebed24545eab344058172fbfbe096539898c70f9
Wilian Ribeiro <wilian087@hotmail.com>
2023-10-18 10:27:19 -0300

    ajuste sequence numerator

--- srv/common/create-sequence-numerators.js
+++ srv/common/create-sequence-numerators.js
@@ -5,10 +5,18 @@ async function OrdersNumerator(req) {
     
     const tx = cds.transaction(req);
             
-    let oOrder       =  await tx.read(cds.services.CatalogService.entities.Orders, ['max(orderNumber)']);
-    let oValueString = oOrder[0]['MAX(ORDERNUMBER)'];
-    let oValue       = Number(oValueString);
-    let oNewValue    = oValue + 1;
+    let oOrder =  await tx.read(cds.services.CatalogService.entities.Orders, ['max(orderNumber)']);
+    
+    let oNewValue = "";
+
+    if(oOrder.length != 0) {
+        let oValueString = oOrder[0]['MAX(ORDERNUMBER)'];
+        let oValue       = Number(oValueString);
+        
+        oNewValue = oValue + 1;
+    }else {
+        oNewValue = "7000000000"; 
+    }
 
     req.data.orderNumber = String(oNewValue);
 
@@ -19,9 +27,17 @@ async function ProposalNumerator(req) {
     const tx = cds.transaction(req);
             
     let oProposal    =  await tx.read(cds.services.CatalogService.entities.Proposal, ['max(proposalNumber)']);
-    let oValueString = oProposal[0]['MAX(PROPOSALNUMBER)'];
-    let oValue       = Number(oValueString);
-    let oNewValue    = oValue + 1;
+
+    let oNewValue = "";
+
+    if(oProposal.length != 0) {
+        let oValueString = oProposal[0]['MAX(PROPOSALNUMBER)'];
+        let oValue       = Number(oValueString);
+        
+        oNewValue = oValue + 1;
+    }else {
+        oNewValue = "8000000000"; 
+    }
 
     req.data.proposalNumber = String(oNewValue);
 
